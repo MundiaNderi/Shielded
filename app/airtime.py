b92cb755d44e2bf3990e17d886cfa6d6b5453309
MundiaNderi <sarahngima77@gmail.com>
2023-06-19 16:43:27 +0300

    airtime.py

--- app/airtime.py
+++ app/airtime.py
@@ -2,24 +2,29 @@
 import africastalking
 import requests
 
+url = "https://api.sandbox.africastalking.com/version1/airtime/send"
+
+
 """Authenticate with the service"""
 username = "sandbox"
 api_key = "58d691a0af4a9082922b4a71deb45f9fc89632f8993260e4679d423b1b5d722f"
-
 africastalking.initialize(username, api_key)
+
+
+"""Create an instance of the Airtime class"""
+airtime = africastalking.Airtime
 
 phone_number = "+254716299581"  # In international format
 currency_code = "KES"  # Change this to your country's code
+amount = 100
 
-
-url = "https://api.sandbox.africastalking.com/version1/airtime/send"
-
-"""Adding headers by passing them as a dictionary"""
-headers = {
-    "apiKey": "58d691a0af4a9082922b4a71deb45f9fc89632f8993260e4679d423b1b5d722f",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Accept": "application/json"
-}
+try:
+    response = airtime.send(phone_number=phone_number,
+                            amount=amount, currency_code=currency_code)
+    print(response)
+except Exception as e:
+    print(
+        "Encountered an error while sending airtime. More error details below\n {e}")
 
 """This data parameter contains the form data as a dictionary"""
 data = {
--- app/consume_api.py
+++ app/consume_api.py
@@ -1,6 +1,20 @@
 import requests
 import json
+import pprint
 
-response = requests.get(
-    'https://api.sandbox.africastalking.com/version1/airtime/send/')
-print(response.json)
+url = 'https://api.sandbox.africastalking.com/version1airtme/send'
+
+parameters = {
+    'api_key': "58d691a0af4a9082922b4a71deb45f9fc89632f8993260e4679d423b1b5d722f",
+    'username': "shielded"
+}
+
+headers = {
+    "api_key": "58d691a0af4a9082922b4a71deb45f9fc89632f8993260e4679d423b1b5d722f",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accepts": "application/json"
+}
+
+response = requests.get(url, params=parameters, headers=headers)
+
+pprint.pprint(response.json())
