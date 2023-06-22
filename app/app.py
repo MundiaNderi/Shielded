f052c6200c11276cb598499f46203efc24a1a940
Lynnmich <lynnenmichuki@gmail.com>
2023-06-22 23:03:59 +0300

    Updates

--- app/app.py
+++ app/app.py
@@ -19,5 +19,26 @@ def ussd_callback():
     text = request.values.get("text", "")
     return handle_ussd_callback(session_id, service_code, phone_number, text, sms, airtime)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/donate', methods=['GET', 'POST'])
+def donate():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        amount = request.form.get('amount')
+        mpesa = request.form.get('mpesa')
+
+        # Process the donation data (e.g., save to a database, initiate payment, etc.)
+
+        return "Thank you for your donation, {}! We received {} KES.".format(name, amount)
+
+    return render_template('donate.html')
+
 if __name__ == '__main__':
     app.run()
